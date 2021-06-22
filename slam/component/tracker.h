36bd1a29be54a9c09e3ded545af7f8650ca38e62
Maksim Kuznetsov <m.a.kuzn@ya.ru>
2021-06-22 17:18:32 +0300

    Add tracker empty functions

--- slam/component/tracker.h
+++ slam/component/tracker.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include "slam/core/frame.h"
+#include <memory>
 namespace slam {
 
+class Tracker {
+ public:
+  Tracker() {
+  }
 
+  void Track(const std::shared_ptr<Frame>& frame) {
+  }
+
+
+};
 
 }  // namespace slam
 
