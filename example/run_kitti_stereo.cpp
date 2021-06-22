0aaeff1702dfd5312e628d489d0b0880befc9f4e
Maksim Kuznetsov <m.a.kuzn@ya.ru>
2021-06-22 17:17:11 +0300

    Change example

--- example/run_kitti_stereo.cpp
+++ example/run_kitti_stereo.cpp
@@ -1,10 +1,17 @@
+#include "slam/util/logger.h"
 #include <memory>
-#include <slam/visual_odometry.h>
+#include <slam/system.h>
+#include <slam/settings.h>
 
 int main(int argc, char** argv) {
+  slam::SLAMSettings settings;
+  settings.SetLogLevel(slam::ELogLevel::kInfo);
+  slam::System slam(settings);
+  /*
   std::shared_ptr<slam::VisualOdometry> vo(
       new slam::VisualOdometry("./config/default.yaml"));
   assert(vo->Init());
   vo->Run();
+  */
 }
 
