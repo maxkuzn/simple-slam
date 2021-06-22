eac330314a5b7d466e55140fea74905a43b9ee72
Maksim Kuznetsov <m.a.kuzn@ya.ru>
2021-06-22 17:17:58 +0300

    Add frame creation

--- slam/core/frame.h
+++ slam/core/frame.h
@@ -7,6 +7,26 @@ namespace slam {
 
 class Frame : public std::enable_shared_from_this<Frame> {
  public:
+  Frame() {
+  }
+
+  static std::shared_ptr<Frame> FromStereo(const cv::Mat& left_img,
+                                           const cv::Mat& right_img,
+                                           double timestamp) {
+    (void) left_img;
+    (void) right_img;
+    (void) timestamp;
+    return std::make_shared<Frame>();
+  }
+
+  static std::shared_ptr<Frame> FromRGBD(const cv::Mat& img,
+                                         const cv::Mat& depth,
+                                         double timestamp) {
+    (void) img;
+    (void) depth;
+    (void) timestamp;
+    return std::make_shared<Frame>();
+  }
 
  private:
   cv::Mat img;
