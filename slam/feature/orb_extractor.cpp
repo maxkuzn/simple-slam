c956e4e13e93ab5b7718c3895d36670a271531ce
Maksim Kuznetsov <m.a.kuzn@ya.ru>
2021-06-23 21:23:58 +0300

    Feature extractor

--- slam/feature/orb_extractor.cpp
+++ slam/feature/orb_extractor.cpp
@@ -1,9 +1,18 @@
 #include "orb_extractor.h"
 
+#include "slam/core/frame.h"
 
 namespace slam {
 
-
+void ORBExtractor::Extract(const cv::Mat& img,
+                           std::vector<cv::KeyPoint>& keypoints,
+                           cv::Mat& descriptors) {
+  detector_->detectAndCompute(
+      img,
+      cv::noArray(),
+      keypoints,
+      descriptors);
+}
 
 }  // namespace slam
 
--- slam/feature/orb_extractor.h
+++ slam/feature/orb_extractor.h
@@ -1,8 +1,27 @@
 #pragma once
 
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <vector>
+// #include <opencv2/xfeatures2d.hpp>
+
 namespace slam {
 
+class Frame;
+
 class ORBExtractor {
+ public:
+  ORBExtractor() {
+    detector_ = cv::ORB::create();
+  }
+
+  void Extract(const cv::Mat& img,
+               std::vector<cv::KeyPoint>& keypoints,
+               cv::Mat& descriptors);
+
+ private:
+  cv::Ptr<cv::ORB> detector_;
 };
 
 
