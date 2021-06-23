98727f27d24cc0d58f16f59d68d21118442b30f6
Maksim Kuznetsov <m.a.kuzn@ya.ru>
2021-06-23 21:22:35 +0300

    Use frame streamer

--- example/run_kitti_stereo.cpp
+++ example/run_kitti_stereo.cpp
@@ -1,17 +1,22 @@
 #include "slam/util/logger.h"
-#include <memory>
-#include <slam/system.h>
-#include <slam/settings.h>
+#include "slam/system.h"
+#include "slam/settings.h"
+#include "slam/frame_streamer/kitti_frame_streamer.h"
 
 int main(int argc, char** argv) {
+  std::string sequence_path = "/home/max/slam/ORB_SLAM2/data/kitti/dataset/sequences/06";
+  slam::KITTIFrameStreamer frame_streamer(sequence_path);
   slam::SLAMSettings settings;
   settings.SetLogLevel(slam::ELogLevel::kInfo);
   slam::System slam(settings);
-  /*
-  std::shared_ptr<slam::VisualOdometry> vo(
-      new slam::VisualOdometry("./config/default.yaml"));
-  assert(vo->Init());
-  vo->Run();
-  */
+
+  size_t iter = 0;
+  for (auto&& frame : frame_streamer) {
+    ++iter;
+    if (iter > 50) {
+      break;
+    }
+    slam.Track(frame);
+  }
 }
 
