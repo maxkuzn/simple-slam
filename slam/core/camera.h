81856c938d6b853dd0ed5cf9e0566921128f7620
Maksim Kuznetsov <m.a.kuzn@ya.ru>
2021-06-24 22:32:41 +0300

    Add K matrix

--- slam/core/camera.h
+++ slam/core/camera.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Eigen/Core>
+
 namespace slam {
 
 struct Camera {
@@ -11,6 +13,14 @@ struct Camera {
       , fx_inv(1.0f / fx_)
       , fy_inv(1.0f / fy_)
   {
+    K << fx,  0, cx,
+          0, fy, cy,
+          0,  0,  1;
+    /*
+    K << 0.0, 0.0, 0.0,
+         0.0, 0.0, 0.0,
+         0.0, 0.0, 0.0;
+    */
   }
 
   const float cx;
@@ -19,6 +29,7 @@ struct Camera {
   const float fy;
   const float fx_inv;
   const float fy_inv;
+  Eigen::Matrix3d K;
 };
 
 }  // namespace slam
